a2d344c6e53032a472fe73a3ed03ce8d0164a187
R21 <r21@example.com>
2022-02-17 15:25:28 -0500

    foo

--- lib/hash_zen/merge.rb
+++ lib/hash_zen/merge.rb
@@ -11,6 +11,15 @@ module HashZen
         end
       end
 
+      # accept options as :icon_name or {code: :icon_name} when key_name: :code
+      # If input is a hash, does not do any checking or modifications
+      # defaults are used as a baseline before doing an HTML merge
+      # Return value is always { code: :icon_name, **opts }
+      def omni_input(input:, key_name: :code, defaults: {}, **opts)
+        mix_hash = input.is_a?(Hash) ? input : { key_name => input }
+        html(base: defaults, mix: mix_hash)
+      end
+
       # HTML style merging
       #  class: combines and append join in the end
       #  data:  combines in to a hash
@@ -31,7 +40,7 @@ module HashZen
           end
           acc
         }
-        rv[:style] = rv[:style].flatten.compact.map { |e| e.to_s.delete_suffix(";") }.join("; ") + ";" if rv.key?(:style)
+        rv[:style] = [rv[:style]].flatten.compact.map { |e| e.to_s.delete_suffix(";") }.join("; ") + ";" if rv.key?(:style)
         Utils.hash_compact(input: rv)
       end
     end
--- test/merge_test.rb
+++ test/merge_test.rb
@@ -19,5 +19,31 @@ module HashZen
       rv = HashZen::Merge.html(base: base, mix: mix)
       assert_nil_equal result, rv
     end
+
+    [
+      { input: :a,                 result: { code: :a }, desc: "Single input, no overrides" },
+      { input: :a, key_name: :div, result: { div: :a }, desc: "key_name specified" },
+      { input: :a, key_name: :div, defaults: {div: :foo}, result: { div: :a }, desc: "key_name specified, default key also specified" },
+      { input: :a, key_name: :div, defaults: {a: 11, b: "X"}, result: { a: 11, b: "X", div: :a }, desc: "key_name specified, default key also specified" },
+    ].each_with_index do |spec, ix|
+      test "omni input #{ix}" do
+        spec = {key_name: :code, defaults: {}}.merge(spec)
+        msg = %Q!With spec: #{spec}\nDesc: #{spec[:desc] ? "" : ""}!
+        rv = HashZen::Merge.omni_input(input: spec[:input], key_name: spec[:key_name], defaults: spec[:defaults])
+        assert_nil_equal spec[:result], rv, msg
+      end
+    end
+
+    test "omni input for icon with svg" do
+      rv = HashZen::Merge.omni_input(input: :close)
+      assert_equal({ code: :close}, rv)
+
+      rv = HashZen::Merge.omni_input(input: { code: :close, class: :foo, style: "padding: 4px" })
+      assert_equal({ code: :close, class: [:foo], style: "padding: 4px;" }, rv)
+
+      rv = HashZen::Merge.omni_input(input: { code: :close, data: { a: 9 }, class: :foo, style: "padding: 4px" },
+      defaults: {code: :xyz, class: :icon, data: { a: 1, b: 2 }, required: true})
+      assert_equal({ code: :close, class: [:icon, :foo], data: { a: 9, b: 2 }, required: true, style: "padding: 4px;" }, rv)
+    end
   end
 end
